16de7a5acb32b7aa79399add5fa2dfe463fba94b
Aybar <you@example.com>
2023-10-11 13:50:36 +0300

    day 25

---
+++ src/day25_İnheritanceDataTypeKullanımı_Overriding/K_GrandParent.java
@@ -0,0 +1,8 @@
+package day25_İnheritanceDataTypeKullanımı_Overriding;
+
+public class K_GrandParent {
+
+    public void method1() {
+        System.out.println("Grandparent method1");
+    }
+}
---
+++ src/day25_İnheritanceDataTypeKullanımı_Overriding/L_Parent.java
@@ -0,0 +1,8 @@
+package day25_İnheritanceDataTypeKullanımı_Overriding;
+
+public class L_Parent extends K_GrandParent {
+
+    public void method1() {
+        System.out.println("Parent method1");
+    }
+}
