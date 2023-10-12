7e8b333cb7893f80301f8aaebde1c790f0f08987
Aybar <you@example.com>
2023-10-12 13:51:41 +0300

    day 26

---
+++ src/day26_Overridin_pollyMorphism/C04_RahatChild.java
@@ -0,0 +1,14 @@
+package day26_Overridin_pollyMorphism;
+
+public class C04_RahatChild extends C03_ZorlamaSevmeyenParent{
+
+    /*
+       Klasik inheritance
+       tüm insiyatifi child class'a bırakır
+
+       child class, parent class'daki class üyelerinden
+       1- istediğini aynen kullanır
+       2- istediğini kendisine uyarlar
+       3- isterse de parent class'da olmayan yeni üyeler ekleyebilir
+     */
+}
